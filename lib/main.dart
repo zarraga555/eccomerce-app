c0a51def79c8f506fc09e5dfa99e7062066d55ef
Alberto Zarraga <zarraga555@gmail.com>
2024-03-08 13:41:42 -0400

    First Commit prueba

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
+        // Prueba
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
