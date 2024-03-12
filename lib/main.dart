8555df6cc7c94fe1c3b1b358561238abd791f7ef
Alberto Zarraga <zarraga555@gmail.com>
2024-03-12 10:51:26 -0400

    Modificaciones a los archivos main

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,17 @@
+import 'package:eccomerce_app/utils/constants/colors.dart';
 import 'package:eccomerce_app/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'app.dart';
 
+// Entry point of flutter App
 void main(){
+
+  // Todo: Add Widgets Binding
+  // Todo: Init Local Storage
+  // Todo: Await Native Splash
+  // Todo: Initialize Firebase
+  // Todo: Initialize Authentication
+
+  
   runApp(const App());
-}
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: EccomerceTheme.lightTheme,
-      darkTheme: EccomerceTheme.darkTheme,
-    );
-  }
 }
