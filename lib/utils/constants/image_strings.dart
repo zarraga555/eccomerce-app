aa5f4e620458430509b2a47799960933486ac910
Alberto Zarraga <zarraga555@gmail.com>
2024-03-12 10:48:39 -0400

    Essential Utilities: Create Files for constants

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,11 @@
+// This class all the App Images in String formats
+class EccomerceImages{
+//   App Logos
+  static const String darkAppLogo = "assets/logos/eccomerce-splash-logo.png";
+  static const String lightAppLogo = "assets/logos/eccomerce-splash-logo.png";
+
+//   Social Logos
+  static const String google = "assets/logos/google-icon.png";
+  static const String facebook = "assets/logos/facebook-icon.png";
+  static const String apple = "assets/logos/apple-icon.png";
+}
