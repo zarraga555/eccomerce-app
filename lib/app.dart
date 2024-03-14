a49a32b8b78b9caaced9724fe1410f866602bbe9
Alberto Zarraga <zarraga555@gmail.com>
2024-03-14 02:58:07 -0400

    Se modifico el archivo para iniciar el onboarding de la app

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,7 @@
+import 'package:eccomerce_app/features/authentication/screens/onboarding/onboarding.dart';
 import 'package:eccomerce_app/utils/theme/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 //Use this Class to setup themes, initial Bindings, any animations an much
 class App extends StatelessWidget {
@@ -7,10 +9,11 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context){
-    return MaterialApp(
+    return GetMaterialApp(
       themeMode: ThemeMode.system,
       theme: EccomerceTheme.lightTheme,
       darkTheme: EccomerceTheme.darkTheme,
+      home: const OnBoardingScreen(),
     );
   }
 }
