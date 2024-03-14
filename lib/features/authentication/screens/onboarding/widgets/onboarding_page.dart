6aebce86b8eca0cc1dddb09bb0d2a34afab9de87
Alberto Zarraga <zarraga555@gmail.com>
2024-03-14 03:01:07 -0400

    Se convirtio en un componente de tipo Widget los screen de los OnBoarding de la aplicacion

---
+++ lib/features/authentication/screens/onboarding/widgets/onboarding_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../utils/constants/sizes.dart';
+import '../../../../../utils/helpers/helper_functions.dart';
+
+class OnBoardingPage extends StatelessWidget {
+  const OnBoardingPage({
+    super.key, required this.image, required this.title, required this.subTitle,
+  });
+
+  final String image, title, subTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(EccomerceSizes.defaultSpace),
+      child: Column(
+        children: [
+          Image(
+            width: EccomerceHelperFunctions.screenWidth() * 0.8,
+            height: EccomerceHelperFunctions.screenHeight() * 0.6,
+            image: AssetImage(image),
+          ),
+          Text(
+            title,
+            style: Theme.of(context).textTheme.headlineMedium,
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(height: EccomerceSizes.spaceBtwItems,),
+          Text(
+            subTitle,
+            style: Theme.of(context).textTheme.bodyMedium,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    );
+  }
+}
