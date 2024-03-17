4e0a6da6e3130ee366da3bfe53bbe66e1f7abc6f
Alberto Zarraga <zarraga555@gmail.com>
2024-03-17 12:28:51 -0400

    Se agrego es espaciado en un archivo diferente

---
+++ lib/common/styles/spacing_styles.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../../utils/constants/sizes.dart';
+
+class EccomerceSpacingStyle {
+  static const EdgeInsetsGeometry paddingWithAppBarHeight = EdgeInsets.only(
+    top: EccomerceSizes.appBarHeight,
+    left: EccomerceSizes.defaultSpace,
+    bottom: EccomerceSizes.defaultSpace,
+    right: EccomerceSizes.defaultSpace,
+  );
+}
