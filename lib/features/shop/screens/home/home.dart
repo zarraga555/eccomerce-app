be83ba2431346f8e342dae1a91fc263324bce5a5
Alberto Zarraga <zarraga555@gmail.com>
2024-03-24 12:58:00 -0400

    Se quito  librerias y se hizo modificaciones de codigo llevandolo en componentes

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,3 +1,4 @@
+import 'package:eccomerce_app/features/shop/screens/home/widgets/home_appbar.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
@@ -7,13 +8,22 @@ class HomeScreen extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
+            /// Header
             EccomercePrimaryHeaderContainer(
-              child: Container(),
+              child: Column(
+                children: [
+                  /// AppBar
+                  EccomerceHomeAppBar()
+                //   Search
+
+                //   Categories
+                ],
               ),
+            ),
           ],
         ),
       ),
