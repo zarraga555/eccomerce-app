750031c66738e77ff6d7f6c99b7505bb708eb23e
Alberto Zarraga <zarraga555@gmail.com>
2024-04-08 23:36:15 -0400

    componente de card horizontal

---
+++ lib/common/widgets/product_cards/product_card_horizontal.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class EccomerceCardHorizontal extends StatelessWidget {
+  const EccomerceCardHorizontal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+  //   Container with side paddings, color, edges, radius and shadow
+    return Container();
+  }
+}
