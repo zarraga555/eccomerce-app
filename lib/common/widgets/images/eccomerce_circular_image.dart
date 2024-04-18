6204629c1f3c91491cc44191a2d93681eee4abd7
Alberto Zarraga <zarraga555@gmail.com>
2024-04-18 12:24:27 -0400

    Se creo este componente con la afinilidad de reutilizar para las images de los brands para saber si es local la imagen o atravez de internet

---
+++ lib/common/widgets/images/eccomerce_circular_image.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import '../../../utils/constants/colors.dart';
+import '../../../utils/constants/sizes.dart';
+import '../../../utils/helpers/helper_functions.dart';
+
+class EccomerceCircularImage extends StatelessWidget {
+  const EccomerceCircularImage({
+    super.key,
+    this.fit = BoxFit.cover,
+    required this.image,
+    this.isNetworkImage = false,
+    this.overlayColor,
+    this.backgroundColor,
+    this.width = 56,
+    this.height = 56,
+    this.padding = EccomerceSizes.sm,
+  });
+
+  final BoxFit? fit;
+  final String image;
+  final bool isNetworkImage;
+  final Color? overlayColor;
+  final Color? backgroundColor;
+  final double width, height, padding;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      padding: EdgeInsets.all(padding),
+      decoration: BoxDecoration(
+        // If Image background color is null switch it to light and dark mode color design
+        color: backgroundColor ?? (EccomerceHelperFunctions.isDarkMode(context) ? EccomerceColors.black : EccomerceColors.white),
+        borderRadius: BorderRadius.circular(100),
+      ),
+      child: Center(
+        child: Image(
+          fit: fit,
+          image: isNetworkImage ? NetworkImage(image) : AssetImage(image) as ImageProvider,
+          color: overlayColor,
+        ),
+      ),
+    );
+  }
+}
